7fb0aa9c70b41e3d937b0bc588a27ee1958bd092
jakub <jkl.school6@gmail.com>
2023-03-26 01:09:51 +0100

    Pierwsze klasy i początkowa symulacja, wstepne zalozenia chyba działają, w czasie pracy generowani sa klienci, którzy wchodzą do "ogólnej" kolejki a ta dysponuje ich do aktywnych kas

--- LogikaKas/Kasa.py
+++ LogikaKas/Kasa.py
@@ -1,20 +1,51 @@
-class Kasa:
-    def __init__(self, valCash: float, active: bool, people: int):
-        self.active = active
-        self.valCash = valCash
-        self.people = people
+from LogikaKas.Klient import Klient
 
-    def getVal(self):
-        return self.valCash
+
+class Kasa:
+
+    def __init__(self, idKasy: int, maxCapacity: int):
+        self.idKasy = idKasy
+        self.active = False
+        self.iloscTransakcji = 0
+        self.wolna = False
+        self.klienci = []
+        self.maxCapacity = maxCapacity
+        self.straznik = Klient
+        self.straznik.czasObslugi = 0
+        self.obslugiwany = self.straznik
+        self.ilePusta = 0  # okresla ile "rund" kasa stoi pusta, po x rundach zamyka kase
+
+    def getIloscTransakcji(self):
+        return self.iloscTransakcji
 
     def getActive(self):
         return self.active
 
-    def setBroke(self):
-        self.active = False
+    def getWolna(self):
+        return self.wolna
 
-    def getPeople(self):
-        return self.people
+    def getKlienci(self):
+        return self.klienci
 
+    def getSize(self):
+        return len(self.klienci)
 
+    def getId(self):
+        return self.idKasy
+
+    def dodajKlienta(self, klient: Klient):
+        if self.getSize() < self.maxCapacity:
+            self.klienci.append(klient)
+            return 1
+        else:
+            return -7
+
+    def obsluzKlienta(self):
+        if not self.getActive(): return -7
+        if self.obslugiwany.czasObslugi == 0 and len(self.klienci) > 0:
+            self.obslugiwany = self.klienci.pop(0) #zabezpieczyc przed popowaniem z pustej listy
+        elif self.obslugiwany.czasObslugi == 0:
+            self.obslugiwany = self.straznik # straznik oznacza ze nikt nie stoi przy kasie
+        else:
+            self.obslugiwany.czasObslugi -= 1
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from LogikaKas.Symulacja import Simulation
 
 if __name__ == "__main__":
-    sim = Simulation()
-    print(sim.lista[0].valCash)
+    sim = Simulation(1)
+
+
 
